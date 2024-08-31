42ef587dc46ed9c3df2f7f2c66f2a4d2b982ae74
jamalihassan0307 <jamalihassan0307@gmail.com>
2024-08-31 09:26:38 +0500

    Initial commit

---
+++ FLUTTER/jammer_mobile_app/lib/pages/my_account/account_setting.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:jammer_mobile_app/pages/login.dart';
+
+class AccountSetting extends StatefulWidget {
+  const AccountSetting({super.key});
+
+  @override
+  _AccountSettingState createState() => _AccountSettingState();
+}
+
+class _AccountSettingState extends State<AccountSetting> {
+  @override
+  Widget build(BuildContext context) {
+    ListTile getListTile(title) {
+      // Widget for common ListTile
+      return ListTile(
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.grey, fontSize: 18.0),
+        ),
+        trailing: const Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.grey,
+        ),
+        onTap: () {},
+      );
+    }
+
+    // Widget for common divider
+    Padding getDivider() {
+      return const Padding(
+        padding: EdgeInsets.only(left: 12.0),
+        child: Divider(
+          height: 1.0,
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF1F3F6),
+      appBar: AppBar(
+        title: const Text('Account Setting'),
+        titleSpacing: 0.0,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            color: Colors.white,
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Account',
+                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+                ),
+                getListTile('Address'),
+                getDivider(),
+                getListTile('Telephone'),
+                getDivider(),
+                getListTile('Email'),
+                getDivider(),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5.0),
+            color: Colors.white,
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Setting',
+                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+                ),
+                getListTile('Order Notifications'),
+                getDivider(),
+                getListTile('Discount Notifications'),
+                getDivider(),
+                getListTile('Credit Card'),
+                getDivider(),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Login()));
+            },
+            child: Container(
+              margin: const EdgeInsets.only(top: 5.0, bottom: 5.0),
+              color: Colors.white,
+              padding: const EdgeInsets.all(20.0),
+              child: const Text(
+                'Logout',
+                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ FLUTTER/jammer_mobile_app/lib/pages/my_account/my_account.dart
@@ -0,0 +1,305 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:jammer_mobile_app/pages/faq_and_about_app/about_app.dart';
+import 'package:jammer_mobile_app/pages/faq_and_about_app/faq.dart';
+
+// My Own Imports
+import 'package:jammer_mobile_app/pages/login.dart';
+import 'package:jammer_mobile_app/pages/my_account/account_setting.dart';
+import 'package:jammer_mobile_app/pages/my_orders.dart';
+import 'package:jammer_mobile_app/pages/notifications.dart';
+import 'package:page_transition/page_transition.dart';
+
+class MyAccount extends StatefulWidget {
+  const MyAccount({super.key});
+
+  @override
+  _MyAccountState createState() => _MyAccountState();
+}
+
+class _MyAccountState extends State<MyAccount> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Account'),
+        titleSpacing: 0.0,
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          SizedBox(
+            width: width,
+            height: 360.0,
+            child: Stack(
+              children: <Widget>[
+                Image(
+                  image: const AssetImage('assets/user_profile/background.jpg'),
+                  width: width,
+                  height: 220.0,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  top: 165.0,
+                  right: ((width / 2) - 50.0),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 110.0,
+                        width: 110.0,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(55.0),
+                          border: Border.all(color: Colors.white, width: 5.0),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50.0),
+                          child: const Image(
+                            image: AssetImage('assets/user_profile/user_3.jpg'),
+                            height: 100.0,
+                            width: 100.0,
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          'Allison Perry',
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: InkWell(
+                          child: const Text(
+                            'Edit Profile',
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.grey),
+                          ),
+                          onTap: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.rightToLeft,
+                      child: const Notifications()));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.bell,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const Text(
+                    'Notifications',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 30.0, left: 70.0),
+            child: Divider(
+              height: 1.0,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.rightToLeft,
+                      child: const MyOrders()));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.truck,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const Text(
+                    'My Orders',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 30.0, left: 70.0),
+            child: Divider(
+              height: 1.0,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.rightToLeft,
+                      child: const AccountSetting()));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.gears,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const Text(
+                    'Account Setting',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 30.0, left: 70.0),
+            child: Divider(
+              height: 1.0,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.rightToLeft,
+                      child: const FaqPage()));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.circleQuestion,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const Text(
+                    'FAQ',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 30.0, left: 70.0),
+            child: Divider(
+              height: 1.0,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.rightToLeft,
+                      child: const AboutApp()));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.info,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const Text(
+                    'About App',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 30.0, left: 70.0),
+            child: Divider(
+              height: 1.0,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Login()));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.rightFromBracket,
+                    size: 30.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const Text(
+                    'Logout',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 30.0, left: 70.0),
+            child: Divider(
+              height: 1.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
